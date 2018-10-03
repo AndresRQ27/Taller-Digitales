c40f05702de61e42aec5e57f9ac194b202c90e2b
Alexis Gavriel <25961885+Frozen25@users.noreply.github.com>
2018-10-03 07:40:32 -0600

    Rename Cable to clock

--- VGA/Application/ParalelRegister.sv
+++ VGA/Application/ParalelRegister.sv
@@ -1,8 +1,8 @@
 module ParalelRegister#(parameter N = 10)
 								(input logic [N-1:0] in,
-								input  logic load,
+								input  logic clock,
 								output logic [N-1:0] out);
-	always_ff@(posedge load)
-		if (load) out <= in;
+	always_ff@(posedge clock)
+		if (clock) out <= in;
 
 endmodule
--- VGA/Application/ParalelRegister_tb.sv
+++ VGA/Application/ParalelRegister_tb.sv
@@ -1,16 +1,16 @@
 module ParalelRegister_tb #(parameter M = 4)();
 
 logic [M-1:0] in , out;
-logic enable;
+logic clock;
 
-ParalelRegister #(M) DUT (in, enable, out);
+ParalelRegister #(M) DUT (in, clock, out);
 
 initial begin
-#1 enable = 1;
+#1 clock = 1;
 #2 in = 4'b0001;
-#2 in = 4'b0010; enable = 0;
+#2 in = 4'b0010; clock = 0;
 #2 in = 4'b0011;
-#2 in = 4'b0100; enable = 1;
+#2 in = 4'b0100; clock = 1;
 #2 in = 4'b1100;
 
 
